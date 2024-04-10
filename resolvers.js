44b7cee8b3802434e1cf0ab27486ae0a64097bcf
Naveena Ponna <Naveena@welkin-digital.com>
2024-04-10 18:31:37 +0530

    code issue

--- resolvers.js
+++ resolvers.js
@@ -1,7 +1,8 @@
 let message = 'Hello, World!';
 let email = 'example@example.com';
 let hello='hello welcome';
-const data = require("./data/data");
+const { TestData } = require("./data/data");
+
 module.exports = {
   getAllData() {
     return TestData;
